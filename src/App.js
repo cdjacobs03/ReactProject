0e4d379a3945322a94b97d0b8e440ccf4ad8be42
Caleb Jacobs <cdjacobs03@icloud.com>
2024-08-09 16:55:58 -0500

    React API

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import atmDefenderImage from './ATMDefender.jpg';
+import './App.css';
+
+function App() {
+  return (
+      <div className="App">
+
+          <img src={atmDefenderImage} alt='ATM Defender' height="125" width="125"/>
+          <h1>ATM Client Defender</h1>
+      </div>
+
+  );
+
+}
+
+export default App;
